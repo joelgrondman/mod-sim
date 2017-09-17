1e82f05653102d6701899bb5323eb228696ddd34
joel <joel@sfgr.nl>
2017-09-17 18:11:38 +0200

    added titles and labels

--- assignment1/chirikov_plot.m
+++ assignment1/chirikov_plot.m
@@ -1,20 +1,36 @@
-K = 1;
-n = 1000;
-orbits = 100;
+K = 1.02;
+n = 10000;
+Inits = [0 0.1;
+         0 0.4;
+         0 0.5;
+         0 0.7777];
+Inits = rand(100,2);
+     
+orbits = size(Inits,1);
 seq = zeros(n,2);
+
 set(gcf, 'Renderer', 'painters')
 %set(gcf, 'Renderer', 'opengl')
 
-Init = rand(2,1);
-
-for i=1:n
-   [Init(1),Init(2)] = chirikov_map(Init(1),Init(2),K);
-   seq(i,:) = Init;
-end
-
-plot(seq(:,1),seq(:,2),'.k','MarkerSize',0.01)
-hold on
+close 1
+figure(1)
+xlabel('x')
+ylabel('p')
 xlim([0 1])
-ylim([0 1]);
+ylim([0 1])
+title(['K=' num2str(K)])
+hold on
+
+for o=1:orbits
+    Init = Inits(o,:);%rand(2,1);
+
+    for i=1:n
+       [Init(1),Init(2)] = chirikov_map(Init(1),Init(2),K);
+       seq(i,:) = Init;
+    end
+
+    plot(seq(:,1),seq(:,2),'.k','MarkerSize',0.01)
+
+end
 
 hold off
